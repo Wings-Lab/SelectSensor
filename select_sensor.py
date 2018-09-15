d510a6c799727c589cdd1acebc16d2764bc1b459
caitaozhan <caitaozhan@gmail.com>
2018-09-15 12:54:47 -0400

    online greedy done

--- select_sensor.py
+++ select_sensor.py
@@ -1,6 +1,7 @@
 '''
 Select sensor and detect transmitter
 '''
+
 import random
 import math
 import copy
@@ -1272,7 +1273,7 @@ class SelectSensor:
         subset_index = []
         complement_index = [i for i in range(self.sen_num)]
         self.print_grid(self.grid_priori)
-        discretize_x = self.discretize(bin_num=400)
+        discretize_x = self.discretize(bin_num=100)
         cost = 0
         subset_to_compute = []
         while cost < budget and complement_index:
